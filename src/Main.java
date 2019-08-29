d4f20966de18e15ae7311146a6c25a30ab8363a9
Josh <jkasanjian@gmail.com>
2019-08-29 11:00:11 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+import javazoom.jl.player.*;
+import javazoom.jl.decoder.JavaLayerException;
+
+
+
+public class Main {
+
+    /**
+     * Play a given audio file.
+     * @param audioFilePath Path of the audio file.
+     */
+    void mp3play(String file) {
+        try {
+            // It uses CECS327InputStream as InputStream to play the song
+            InputStream is = new CECS327InputStream(file);
+            Player mp3player = new Player(is);
+            mp3player.play();
+        }
+        catch (JavaLayerException exception)
+        {
+            exception.printStackTrace();
+        }
+        catch (IOException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Integer i;
+        Main player = new Main();
+        player.mp3play("imperial.mp3");
+    }
+
+}
+
