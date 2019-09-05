43ec843f73dbd9899076f023357fd3c92f51ebe5
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2019-09-05 11:23:36 -0700

    modified src file

---
+++ src/model/profile/Playlist.java
@@ -0,0 +1,6 @@
+package model.profile;
+
+public class Playlist {
+
+
+}
---
+++ src/model/profile/Profile.java
@@ -0,0 +1,39 @@
+package model.profile;
+
+public class Profile {
+
+    private String username;
+    private String password;
+    private Playlist playlist;
+
+    public Profile(String username, String password, Playlist playlist) {
+        this.username = username;
+        this.password = password;
+        this.playlist = playlist;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Playlist getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+}
