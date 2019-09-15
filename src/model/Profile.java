81943b0b73ce24819693177711e400d59447d131
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2019-09-15 11:57:07 -0700

    added Profiles class and GSON parser

--- src/model/Profile.java
+++ src/model/Profile.java
@@ -12,6 +12,11 @@ public class Profile {
         this.playlist = playlist;
     }
 
+    public Profile(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
