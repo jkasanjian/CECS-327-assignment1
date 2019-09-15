8984f447e5c4f4885f5959a565be48d27d33bfb6
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2019-09-15 12:18:27 -0700

    added static profile class that references profileaccounts

---
+++ src/model/ProfileAccounts.java
@@ -0,0 +1,110 @@
+package model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class ProfileAccounts {
+
+    private final String FILE_NAME = "profiles.json";
+
+    private List<Profile> profiles;
+
+    public ProfileAccounts() {
+        loadProfiles();
+    }
+
+    public void deleteProfile(String username) {
+        profiles.removeIf(u -> (u.getUsername().equals(username)));
+        saveProfiles(profiles);
+    }
+
+    public boolean contains(String username) {
+        for(Profile profile : profiles) {
+            if(profile.getUsername().equalsIgnoreCase(username))
+                return true;
+        }
+        return false;
+    }
+
+    public List<Profile> getProfiles() {
+        return profiles;
+    }
+
+    public boolean addProfile(Profile profile) {
+        if (contains(profile.getUsername())) {
+            return false;
+        }
+        Gson gson = new Gson();
+        File file = new File(FILE_NAME);
+        String currJson = "";
+        String newJson;
+        if(file.exists() && file.length() > 0) {
+            try {
+                Scanner sc = new Scanner(file);
+                while(sc.hasNextLine())
+                    currJson += sc.nextLine();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+        try {
+            List<Profile> oldList;
+            List<Profile> newList = new ArrayList<>();
+            if(file.exists() && file.length() > 0) {
+                oldList = gson.fromJson(currJson, new TypeToken<List<Profile>>(){}.getType());
+                newList.addAll(oldList);
+            }
+            newList.add(profile);
+            newJson = gson.toJson(newList);
+            PrintWriter printWriter = new PrintWriter(file);
+            printWriter.print(newJson);
+            printWriter.close();
+            profiles = newList;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return true;
+    }
+
+    public void saveProfiles(List<Profile> profiles) {
+        Gson gson = new Gson();
+        File file = new File(FILE_NAME);
+        assert profiles != null;
+        PrintWriter printWriter = null;
+        try {
+            printWriter = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        printWriter.print(gson.toJson(profiles));
+        printWriter.close();
+    }
+
+    public void loadProfiles() {
+        Gson gson = new Gson();
+        File file = new File(FILE_NAME);
+        assert file.exists();
+        try {
+            Scanner sc = new Scanner(file);
+            String jsonString = "";
+            while(sc.hasNextLine()) {
+                jsonString += sc.nextLine();
+            }
+            profiles = gson.fromJson(jsonString, new TypeToken<List<Profile>>(){}.getType());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
