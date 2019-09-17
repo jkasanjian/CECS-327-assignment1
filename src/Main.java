f4ed5e14190f857cb3aa8d61c48a19f7cb96486a
Josh <jkasanjian@gmail.com>
2019-09-17 11:29:58 -0700

    merging

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,27 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import javazoom.jl.player.*;
-import javazoom.jl.decoder.JavaLayerException;
+package sample;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
+// Display User
+// Show all Playlists
+// Global Playlist
+// Display all song attributes
 
-public class Main {
+public class Main extends Application {
 
-    /**
-     * Play a given audio file.
-     //* @param audioFilePath Path of the audio file.
-     */
-    void mp3play(String file) {
-        try {
-            // It uses CECS327InputStream as InputStream to play the song
-            InputStream is = new CECS327InputStream(file);
-            Player mp3player = new Player(is);
-            mp3player.play();
-        }
-        catch (JavaLayerException exception)
-        {
-            exception.printStackTrace();
-        }
-        catch (IOException exception)
-        {
-            exception.printStackTrace();
-        }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        primaryStage.setTitle("Home Page");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
-
 
     public static void main(String[] args) {
-        ArrayList<MusicClass> master = readMusicJSON();
-
-        // testing
-        MusicClass song1 = master.get(0);
-        System.out.println(song1.getSongTitle());
-        System.out.println(song1.getSongArtist());
-        System.out.println(song1.getSongID());
-
+        launch(args);
     }
-
-
-    public static ArrayList<MusicClass> readMusicJSON(){
-        Gson gson = new Gson();
-        String fileName = "music.json";
-
-        try{
-
-            Type musicClassType = new TypeToken<ArrayList<MusicClass>>(){}.getType();
-            ArrayList<MusicClass> musicList = gson.fromJson(new FileReader(fileName), musicClassType);
-            // musicList has ArrayList of MusicClass objects
-             return musicList;
-        }
-        catch(FileNotFoundException e){
-            System.out.println("File not found.");
-        }
-        return new ArrayList<>();
-    }
-
-
-
-
-
 }
-
