d4df111054a3e98351aa312b17beeeb9bb5f2675
Anthony Guerra <agaa2492@hotmail.com>
2019-10-10 11:44:43 -0700

    changed communication module to work asynchronously with input and output queues

--- server/src/CommunicationModule.java
+++ server/src/CommunicationModule.java
@@ -1,3 +1,4 @@
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -10,24 +11,54 @@ public class CommunicationModule{
     private Dispatcher dispatcher;
     private DatagramSocket socket;
     private DatagramPacket packet;
-    private byte[] buffer;
-    private byte[] service;
-    private Queue<DatagramPacket> queue;
+    private Queue<DatagramPacket> outgoing_queue;
+    private Queue<DatagramPacket> incoming_queue;
     //TODO: Create history with a hashmap.
 
+    private class HandlePackets implements Runnable{
+        private DatagramPacket dp;
+        private byte[] service;
+
+        public HandlePackets(DatagramPacket data){
+            dp = data;
+        }
+
+        public void run(){
+            String received = new String( dp.getData(), 0, dp.getLength() );
+            String response = dispatcher.dispatch( received );
+
+            InetAddress clientAddress = dp.getAddress();
+            int port = dp.getPort();
+            service  = response.getBytes();
+            DatagramPacket responsePacket   = new DatagramPacket( service, service.length, clientAddress, port );
+            synchronized ( outgoing_queue ){
+                outgoing_queue.add(responsePacket);
+            }
+        }
+    }
+
     public CommunicationModule(int port, Dispatcher dispatch) throws IOException{
-        buffer = new byte[FRAGMENT_SIZE];
-        queue  = new LinkedList<>();
+        outgoing_queue = new LinkedList<>();
+        incoming_queue = new LinkedList<>();
 
         socket     = new DatagramSocket( port );
-        packet     = new DatagramPacket( buffer,  buffer.length );
         dispatcher = dispatch;
 
         new Thread( () -> {
             while(true){
-                synchronized ( queue ){
-                    if( !queue.isEmpty() ){
+                synchronized ( outgoing_queue ){
+                    if( !outgoing_queue.isEmpty() ){
                         send();
+                    }
+                }
+            }
+        }).start();
+
+        new Thread( () -> {
+            while(true){
+                synchronized ( incoming_queue ){
+                    if( !incoming_queue.isEmpty() ){
+                        receive();
                     }
                 }
             }
@@ -36,28 +67,31 @@ public class CommunicationModule{
 
     public void listen() throws IOException{
         while(true){
+            byte [] buffer = new byte[FRAGMENT_SIZE];
+            DatagramPacket packet = new DatagramPacket( buffer, 0, buffer.length );
             socket.receive( packet );
-            String received = new String( packet.getData(), 0, packet.getLength() );
 
-
-            new Thread( () -> {
-                String response = dispatcher.dispatch( received );
-                InetAddress clientAddress = packet.getAddress();
-                int port = packet.getPort();
-                service  = response.getBytes();
-                packet   = new DatagramPacket( service, service.length, clientAddress, port );
-                synchronized ( queue ){
-                    queue.add(packet);
-                }
-            }).start();
+            synchronized ( incoming_queue ){
+                incoming_queue.add(packet);
+            }
         }
+    }
+
+    public void receive(){
+        DatagramPacket pack;
+        synchronized ( incoming_queue ){
+            pack = incoming_queue.remove();
+        }
+
+        Runnable run = new HandlePackets(pack);
+        new Thread(run).start();
     }
 
     private void send(){
         DatagramPacket response;
         try{
-            synchronized ( queue ){
-                response = queue.poll();
+            synchronized ( outgoing_queue ){
+                response = outgoing_queue.poll();
             }
 
             socket.send( response );
